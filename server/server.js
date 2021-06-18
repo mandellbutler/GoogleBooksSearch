51556a126dfac3a3e57ae586246a23454bbc3ca0
Mandell Butler <mandell.butler@outlook.com>
2021-06-18 19:35:11 -0400

    Updated server.js file

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
